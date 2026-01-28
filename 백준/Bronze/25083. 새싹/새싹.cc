5adf23cae1e4003bb0f4006461c92ceab4a02423
kaila <130740278+SeoGyeongmi@users.noreply.github.com>
2026-01-28 21:08:45 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char s1[50] = "         ,r\'\"7";
+    char s2[50] = "r`-_   ,\'  ,/";
+    char s3[50] = " \\. \". L_r\'";
+    char s4[50] = "   `~\\/";
+    char s5[50] = "      |";
+    char s6[50] = "      |";
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    printf("%s\n", s3);
+    printf("%s\n", s4);
+    printf("%s\n", s5);
+    printf("%s", s6);
+    
+    return 0;
+}
+/*
+
+*/
