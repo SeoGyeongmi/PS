6d743fa8b2f6a5642b8b9009487d38156d032790
kaila <130740278+SeoGyeongmi@users.noreply.github.com>
2026-01-28 17:19:56 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 64 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x;
+    int y;
+} Point;
+
+int cmp(const void* a, const void* b){
+    int x1 = ((Point*)a)->x;
+    int y1 = ((Point*)a)->y;
+    int x2 = ((Point*)b)->x;
+    int y2 = ((Point*)b)->y;
+
+    if(x1!=x2){ // x좌표가 다르다면 -> 바로 구분가능
+        if(x1 > x2) return 1;
+        else return -1;
+    } 
+    else{ // x좌표가 서로 같다면 -> y좌표로 구분하기
+        if(y1 > y2) return 1;
+        if(y1 < y2) return -1;
+        else return 0;
+    }
+ 
+}
+int main() {
+    // The number of coordinates is input.
+    int noc;
+    scanf("%d", &noc);
+    Point *p = (Point*)malloc(sizeof(Point)*noc);
+
+    for(int i=0; i<noc; i++){
+        scanf("%d %d", &(p[i].x), &(p[i].y));
+    }
+
+    qsort(p, noc, sizeof(Point), cmp);
+
+    for(int i=0; i<noc; i++){
+        printf("%d %d\n", p[i].x, p[i].y);
+    }
+
+
+    return 0;
+
+}
